95389049b9def0b130a7fce30f5a359e730079e2
Lasagnanator <d96a07@gmail.com>
2022-07-30 19:36:04 +0200

    Moved around polybar configs, added second bar on bottom

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -2,4 +2,5 @@
 
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 0.5; done
-polybar -c ~/.config/polybar/config.ini main &
+polybar -c ~/.config/polybar/config.ini top &
+polybar -c ~/.config/polybar/config.ini bottom &
