1bbbfa6b266ff26c7a2235bd3280f7cacf85e74d
Lasagnanator <d96a07@gmail.com>
2022-08-21 21:19:06 +0000

    Added rudimentary neovim keybinds, added cfacts and layouts to dwm

---
+++ nvim/lua/remap.lua
@@ -0,0 +1,13 @@
+vim.g.mapleader=" "
+
+vim.cmd("nnoremap <Leader>v <C-W>v")
+vim.cmd("nnoremap <Leader>s <C-W>s")
+vim.cmd("nnoremap <Leader>l <C-W>l")
+vim.cmd("nnoremap <Leader>h <C-W>h")
+vim.cmd("nnoremap <Leader>L <C-W>L")
+vim.cmd("nnoremap <Leader>H <C-W>H")
+vim.cmd("nnoremap <Leader>o <C-W>o")
+vim.cmd("nnoremap <Leader><bar> <C-W><bar>")
+vim.cmd("nnoremap <Leader>_ <C-W>_")
+vim.cmd("nnoremap <Leader>t gt")
+vim.cmd("nnoremap <Leader>T gT")
