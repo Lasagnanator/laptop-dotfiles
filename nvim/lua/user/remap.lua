ff1221227ef590eea6b150b69a14689fd226bb23
Lasagnanator <d96a07@gmail.com>
2022-10-17 16:32:48 +0200

    Organized remaps entries and added more functions for tabs, windows and visual mode (visual-block still wip)

--- nvim/lua/user/remap.lua
+++ nvim/lua/user/remap.lua
@@ -3,52 +3,65 @@ vim.g.maplocalleader = " "
 
 local options = { noremap = true, silent = false }
 local keymap = vim.keymap.set
+local apimap = vim.api.nvim_set_keymap
 
 keymap( "", "<Space>", "<Nop>", options )
 
+--< NORMAL MODE >--
 
-keymap( "n", "<Leader>w",     "<Cmd>w<CR>",      options )
-keymap( "n", "<Leader>q",     "<Cmd>q!<CR>",     options )
-keymap( "n", "<Leader>W",     "<Cmd>wa<CR>",     options )
-keymap( "n", "<Leader>Q",     "<Cmd>qa!<CR>",    options )
-keymap( "n", "<Leader>e",     ":e ",             options )
-keymap( "n", "<Leader>v",     "<C-W>v<C-W>l:e ", options )
-keymap( "n", "<Leader>s",     "<C-W>s<C-W>j:e ", options )
-keymap( "n", "<C-h>",         "<C-W>h",          options )
-keymap( "n", "<C-j>",         "<C-W>j",          options )
-keymap( "n", "<C-k>",         "<C-W>k",          options )
-keymap( "n", "<C-l>",         "<C-W>l",          options )
-keymap( "n", "<C-Left>",      "<C-W>H",          options )
-keymap( "n", "<C-Down>",      "<C-W>J",          options )
-keymap( "n", "<C-Up> ",       "<C-W>K",          options )
-keymap( "n", "<C-Right>",     "<C-W>L",          options )
-keymap( "n", "<Leader>o",     "<C-W>o",          options )
-keymap( "n", "<Leader><bar>", "<C-W><bar>",      options )
-keymap( "n", "<Leader>_",     "<C-W>_",          options )
-keymap( "n", "<Leader>n",     ":tabnew ",        options )
-keymap( "n", "<Leader>t",     "gt",              options )
-keymap( "n", "<Leader>T",     "gT",              options )
-              
---vim.cmd("nnoremap <Leader>w <Cmd>w<CR>")
---vim.cmd("nnoremap <Leader>q <Cmd>q!<CR>")
---vim.cmd("nnoremap <Leader>W <Cmd>wa<CR>")
---vim.cmd("nnoremap <Leader>Q <Cmd>qa!<CR>")
---vim.cmd("nnoremap <Leader>e :e ")
---vim.cmd("nnoremap <Leader>v <C-W>v<C-W>l:e ")
---vim.cmd("nnoremap <Leader>s <C-W>s<C-W>j:e ")
---vim.cmd("nnoremap <C-h> <C-W>h")
---vim.cmd("nnoremap <C-j> <C-W>j")
---vim.cmd("nnoremap <C-k> <C-W>k")
---vim.cmd("nnoremap <C-l> <C-W>l")
---vim.cmd("nnoremap <C-Left> <C-W>H")
---vim.cmd("nnoremap <C-Down> <C-W>J")
---vim.cmd("nnoremap <C-Up> <C-W>K")
---vim.cmd("nnoremap <C-Right> <C-W>L")
---vim.cmd("nnoremap <Leader>o <C-W>o")
---vim.cmd("nnoremap <Leader><bar> <C-W><bar>")
---vim.cmd("nnoremap <Leader>_ <C-W>_")
---vim.cmd("nnoremap <Leader>n :tabnew ")
---vim.cmd("nnoremap <Leader>t gt")
---vim.cmd("nnoremap <Leader>T gT")
--- vim.cmd("nnoremap <Leader>v :vsplit ")
--- vim.cmd("nnoremap <Leader>s :split ")
+-- Base
+keymap( "n", "<Leader>w",     ":w<CR>",                  options )
+keymap( "n", "<Leader>q",     ":q!<CR>",                 options )
+keymap( "n", "<Leader>W",     ":wa<CR>",                 options )
+keymap( "n", "<Leader>Q",     ":qa!<CR>",                options )
+keymap( "n", "<Leader>e",     ":e ",                     options )
+
+-- Window management
+keymap( "n", "<Leader>v",     ":vsplit ",                options )
+keymap( "n", "<Leader>s",     ":split ",                 options )
+keymap( "n", "<Leader>o",     "<C-W>o",                  options )
+keymap( "n", "<Leader><bar>", "<C-W><bar>",              options )
+keymap( "n", "<Leader>_",     "<C-W>_",                  options )
+
+-- Move between windows
+keymap( "n", "<C-h>",         "<C-W>h",                  options )
+keymap( "n", "<C-j>",         "<C-W>j",                  options )
+keymap( "n", "<C-k>",         "<C-W>k",                  options )
+keymap( "n", "<C-l>",         "<C-W>l",                  options )
+
+-- Move windows
+keymap( "n", "<A-h>",         "<C-W>H",                  options )
+keymap( "n", "<A-j>",         "<C-W>J",                  options )
+keymap( "n", "<A-k>",         "<C-W>K",                  options )
+keymap( "n", "<A-l>",         "<C-W>L",                  options )
+
+-- Resize windows
+keymap( "n", "<C-Left>",      ":vertical resize -2<CR>", options )
+keymap( "n", "<C-Right>",     ":vertical resize +2<CR>", options )
+keymap( "n", "<C-Up>",        ":resize +2<CR>",          options )
+keymap( "n", "<C-Down>",      ":resize -2<CR>",          options )
+
+-- Tabs
+keymap( "n", "<Leader>t",     ":tabnew ",                options )
+keymap( "n", "<A-PageUp>",    ":+tabmove<CR>",           options )
+keymap( "n", "<A-PageDown>",  ":-tabmove<CR>",           options )
+
+--< VISUAL MODE >--
+
+-- Normal
+keymap( "v", ">",     ">gv",             options )
+keymap( "v", "<",     "<gv",             options )
+keymap( "v", "p",     '"_dP',            options )
+keymap( "v", "<A-j>", ":move .+1<CR>==", options )
+keymap( "v", "<A-k>", ":move .-1<CR>==", options )
+
+-- Block
+keymap( "x", "<A-j>", ":move '>+1<CR>gv-gv", options )
+keymap( "x", "<A-k>", ":move '<-2<CR>gv-gv", options )
+
+--< ALTERNATIVES >--
+
+-- keymap( "n", "<Leader>v",     "<C-W>v<C-W>l:e ", options )
+-- keymap( "n", "<Leader>s",     "<C-W>s<C-W>j:e ", options )
+-- keymap( "x", "<A-h>", ":move '>+1<CR>gv-gv", options )       -- Need to move text laterally
+-- keymap( "x", "<A-l>", ":move '<-2<CR>gv-gv", options )       -- Need to move text laterally
