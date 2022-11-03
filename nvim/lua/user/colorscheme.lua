620e53f676623589151e4a14db15bea19d88ba79
Lasagnanator <d96a07@gmail.com>
2022-11-03 16:07:20 +0100

    Reformatted colorscheme file

--- nvim/lua/user/colorscheme.lua
+++ nvim/lua/user/colorscheme.lua
@@ -1,13 +1,15 @@
--- Selected colorscheme
-local colorscheme = "tokyonight"
+--<< COLORSCHEME & THEMEING >>--
 
--- Manage error if colorscheme not found
+--<< Selected colorscheme
+local colorscheme = "tokyonight-night"
+
+--<< Manage error if colorscheme not found
 local status_ok, _ = pcall ( vim.cmd, "colorscheme " .. colorscheme )
 if not status_ok then
     vim.notify("Colorscheme " .. colorscheme .. "not found, loading default")
     return
 end
 
--- Set background to transparent
-vim.cmd("highlight Normal guibg=none cterm=none")
-vim.cmd("highlight NonText guibg=none cterm=none")
+--<< Set background to transparent
+-- vim.cmd("highlight Normal guibg=none cterm=none")
+-- vim.cmd("highlight NonText guibg=none cterm=none")
