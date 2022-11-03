d79aa315c104bc6128a5130ca4dc278f6a64ee8c
Lasagnanator <d96a07@gmail.com>
2022-11-04 00:17:33 +0100

    Added and configured completion and snippet engine

--- nvim/lua/user/configs/cmp.lua
+++ nvim/lua/user/configs/cmp.lua
@@ -1,75 +1,160 @@
+--<< COMPLETION PLUGIN >>--
+
+--<< Protected calls
 local cmp_status_ok, cmp = pcall(require, "cmp")
 if not cmp_status_ok then
-  return
+    return
 end
 
 local snip_status_ok, luasnip = pcall(require, "luasnip")
 if not snip_status_ok then
-  return
+    return
 end
 
+--<< Kind icons definition
+local kind_icons = {
+  Text          = " ",
+  Method        = "m ",
+  Function      = " ",
+  Constructor   = " ",
+  Field         = " ",
+  Variable      = " ",
+  Class         = " ",
+  Interface     = " ",
+  Module        = " ",
+  Property      = " ",
+  Unit          = " ",
+  Value         = " ",
+  Enum          = " ",
+  Keyword       = " ",
+  Snippet       = " ",
+  Color         = " ",
+  File          = " ",
+  Reference     = " ",
+  Folder        = " ",
+  EnumMember    = " ",
+  Constant      = " ",
+  Struct        = " ",
+  Event         = " ",
+  Operator      = " ",
+  TypeParameter = " ",
+}
+
+--<< Plugin setup
 cmp.setup({
-    snippet = {
-        -- REQUIRED - you must specify a snippet engine
+    snippet = { -- Snippet engine call
         expand = function(args)
-            -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-            luasnip.lsp_expand(args.body) -- For `luasnip` users.
-            -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+            luasnip.lsp_expand(args.body)
         end,
     },
-    window = {
-        -- completion = cmp.config.window.bordered(),
-        -- documentation = cmp.config.window.bordered(),
+    window = { -- Borders
+        -- completion    = cmp.config.window.bordered("single"),
+        -- documentation = cmp.config.window.bordered("single"),
     },
-    mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    mapping = cmp.mapping.preset.insert({ -- Keybinds
+        ["<C-k>"]     = cmp.mapping.select_prev_item(),
+        ["<C-j>"]     = cmp.mapping.select_next_item(),
+        ['<C-b>']     = cmp.mapping.scroll_docs(-2),
+        ['<C-f>']     = cmp.mapping.scroll_docs(2),
         ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<C-e>"]     = cmp.mapping.abort(),
+        ["<CR>"]      = cmp.mapping.confirm({ select = false }), -- False requires selection, true autoselects first result
+        ["<Tab>"]     = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+        ["<S-Tab>"]   = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
     }),
-    sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-        -- { name = 'vsnip' }, -- For vsnip users.
-        { name = 'luasnip' }, -- For luasnip users.
-        -- { name = 'ultisnips' }, -- For ultisnips users.
-        -- { name = 'snippy' }, -- For snippy users.
+    formatting = {
+        fields = { "kind", "abbr", "menu" },
+        format = function(entry, vim_item)
+            vim_item.kind = string.format("%s", kind_icons[vim_item.kind]) -- Kind icons
+            vim_item.menu = ({ -- Menu items name
+                nvim_lsp      = "[LSP]",
+                luasnip       = "[Snippet]",
+                buffer        = "[Buffer]",
+                path          = "[Path]",
+                nerdfont      = "[NerdFont]",
+                emoji         = "[Emoji]",
+                latex_symbols = "[LaTeX]",
+            })[entry.source.name]
+            return vim_item
+        end,
+    },
+    sources = cmp.config.sources({ -- Sources for all files
+        { name = "luasnip" },
+        { name = "nvim_lsp" },
     }, {
-            { name = 'buffer' },
+        { name = "buffer" },
+    })
+})
+
+--<< Configuration for tex files
+cmp.setup.filetype({ "tex", "plaintex" }, {
+    sources = cmp.config.sources({
+        { name = "latex_symbols" },
+        { name = "nerdfont" },
+        { name = "emoji" },
+    })
+})
+
+--<< Configuration for markdown files
+cmp.setup.filetype("markdown", {
+    sources = cmp.config.sources({
+        { name = "nerdfont" },
+        { name = "emoji" },
+    })
+})
+
+--<< Configuration for gitcommit files
+cmp.setup.filetype("gitcommit", {
+    sources = cmp.config.sources({
+        { name = "cmp_git" }, -- You can specify the `cmp_git` source if you were installed it.
+    }, {
+            { name = "buffer" },
         })
 })
 
--- Set configuration for specific filetype.
-cmp.setup.filetype('gitcommit', {
-    sources = cmp.config.sources({
-        { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-    }, {
-            { name = 'buffer' },
-        })
-})
-
--- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline({ '/', '?' }, {
+--<< Use buffer source for `/` and `?` (if you enabled `native_menu`, this won"t work anymore).
+cmp.setup.cmdline({ "/", "?" }, {
     mapping = cmp.mapping.preset.cmdline(),
     sources = {
-        { name = 'buffer' }
+        { name = "buffer" }
     }
 })
 
--- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
+--<< Use cmdline & path source for ":" (if you enabled `native_menu`, this won"t work anymore).
+cmp.setup.cmdline({ ":", "!" }, {
     mapping = cmp.mapping.preset.cmdline(),
     sources = cmp.config.sources({
-        { name = 'path' }
+        { name = "path" }
     }, {
-            { name = 'cmdline' }
-        })
+        { name = "cmdline" },
+        { name = "buffer" }
+    })
 })
 
--- -- Set up lspconfig.
--- local capabilities = require('cmp_nvim_lsp').default_capabilities()
--- -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
--- require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
---     capabilities = capabilities
--- }
+--<< Set up lspconfig (needed?)
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+local lspconfig    = require("lspconfig")
+local mason        = require("mason-lspconfig")
+local servers      = mason.get_installed_servers()
+
+for _, server in ipairs(servers) do
+    lspconfig[server].setup({
+        capabilities = capabilities
+    })
+end
