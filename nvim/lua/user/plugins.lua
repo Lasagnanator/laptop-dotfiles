a61c9175bb320c6697cbed49877cfabfb386c8f7
Andrea Divo <55412507+Lasagnanator@users.noreply.github.com>
2022-11-05 20:16:58 +0000

    Added deactivated plugins for future use

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -82,8 +82,13 @@ return require("packer").startup({
         use "lunarvim/synthwave84.nvim" -- Synthwave '84 Neovim theme
 
         --<< Deactivated
-        -- use "glepnir/dashboard-nvim"                        -- Customize the opening screen
-        -- use "hrsh7th/cmp-emoji"                             -- Completion for emojis
+        -- use "glepnir/dashboard-nvim" -- Customize the opening screen
+        -- use "hrsh7th/cmp-emoji" -- Completion for emojis
+        -- use "kristijanhusak/vim-dadbod-completion" -- Completion for database interactions
+        -- use "epwalsh/obsidian.nvim" -- Write and Navigate Obsidian vaults
+        -- use "preservim/tagbar" -- Open a list of components on the side
+        -- use "frioux/vim-lost" -- Plugin to check, not sure if useful
+
 
         -- Run PackerSync if bootstrapped
         if packer_bootstrap then
