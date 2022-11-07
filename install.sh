c0e949b417c2ade23aa346af7893ca9aebcc4096
Lasagnanator <d96a07@gmail.com>
2022-11-07 10:38:30 +0100

    Corrected preliminary install script

--- install.sh
+++ install.sh
@@ -4,16 +4,16 @@
 # TODO: install packages based on system
 # For now just link proper folders in .config folder
 
-rm -rf $HOME/.config/awesome/
-rm -rf $HOME/.config/kitty/
-rm -rf $HOME/.config/nvim/
-rm -rf $HOME/.config/picom/
-rm -rf $HOME/.config/rofi/
-rm -rf $HOME/.config/wallpapers/
+rm -rf ${HOME}/.config/awesome
+rm -rf ${HOME}/.config/kitty
+rm -rf ${HOME}/.config/nvim
+rm -rf ${HOME}/.config/picom
+rm -rf ${HOME}/.config/rofi
+rm -rf ${HOME}/.config/wallpapers
 
-ln -s ./awesome/ $HOME/.config/
-ln -s ./kitty/ $HOME/.config/
-ln -s ./nvim/ $HOME/.config/
-ln -s ./picom/ $HOME/.config/
-ln -s ./rofi/ $HOME/.config/
-ln -s ./wallpapers/ $HOME/.config/
+ln -s ${PWD}/awesome ${HOME}/.config/
+ln -s ${PWD}/kitty ${HOME}/.config/
+ln -s ${PWD}/nvim ${HOME}/.config/
+ln -s ${PWD}/picom ${HOME}/.config/
+ln -s ${PWD}/rofi ${HOME}/.config/
+ln -s ${PWD}/wallpapers ${HOME}/.config/
