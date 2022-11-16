51b562b2c2dffc031bfaf5934c57f405d465f527
Lasagnanator <d96a07@gmail.com>
2022-11-16 17:11:04 +0100

    Changed <CMD> to <Cmd> to respect docs

--- nvim/lua/user/remap.lua
+++ nvim/lua/user/remap.lua
@@ -13,72 +13,71 @@ keymap( "", "<Space>", "<Nop>", silent )
 --<< NORMAL MODE >>--
 
 --<< Base
-keymap( "n", "<Leader>w",     "<CMD>w<CR>",                  silent )
-keymap( "n", "<Leader>q",     "<CMD>q!<CR>",                 silent )
-keymap( "n", "<Leader>QW",    "<CMD>wa<CR>\
-                               <CMD>qa<CR>",                 silent )
-keymap( "n", "<Leader>W",     "<CMD>wa<CR>",                 silent )
-keymap( "n", "<Leader>QQ",     "<CMD>qa!<CR>",                silent )
-keymap( "n", "<Leader>@",     "<CMD>set cursorcolumn!<CR>",  silent )
-keymap( "n", "<C-s>",         "<CMD>saveas ",                nosilent )
+keymap( "n", "<Leader>w",     "<Cmd>w<CR>",                  silent )
+keymap( "n", "<Leader>q",     "<Cmd>q!<CR>",                 silent )
+keymap( "n", "<Leader>QW",    "<Cmd>wa<CR>\
+                               <Cmd>qa<CR>",                 silent )
+keymap( "n", "<Leader>W",     "<Cmd>wa<CR>",                 silent )
+keymap( "n", "<Leader>QQ",    "<Cmd>qa!<CR>",                silent )
+keymap( "n", "<Leader>@",     "<Cmd>set cursorcolumn!<CR>",  silent )
+keymap( "n", "<C-s>",         ":saveas ",                    nosilent )
 keymap( "n", "<S-k>",         "k<S-j>",                      silent )
 
 --<< Window management
--- keymap( "n", "<Leader>v",     "<CMD>vsplit ",                nosilent )
--- keymap( "n", "<Leader>s",     "<CMD>split ",                 nosilent )
-keymap( "n", "<Leader>o",     "<CMD>wincmd o<CR>",           silent )
-keymap( "n", "<Leader><bar>", "<CMD>wincmd <bar><CR>",       silent )
-keymap( "n", "<Leader>_",     "<CMD>wincmd _<CR>",           silent )
+keymap( "n", "<Leader>o",     "<Cmd>wincmd o<CR>",           silent )
+keymap( "n", "<Leader><bar>", "<Cmd>wincmd <bar><CR>",       silent )
+keymap( "n", "<Leader>_",     "<Cmd>wincmd _<CR>",           silent )
 
 --<< Move between windows
-keymap( "n", "<C-h>",         "<CMD>wincmd h<CR>",           silent )
-keymap( "n", "<C-j>",         "<CMD>wincmd j<CR>",           silent )
-keymap( "n", "<C-k>",         "<CMD>wincmd k<CR>",           silent )
-keymap( "n", "<C-l>",         "<CMD>wincmd l<CR>",           silent )
+keymap( "n", "<C-h>",         "<Cmd>wincmd h<CR>",           silent )
+keymap( "n", "<C-j>",         "<Cmd>wincmd j<CR>",           silent )
+keymap( "n", "<C-k>",         "<Cmd>wincmd k<CR>",           silent )
+keymap( "n", "<C-l>",         "<Cmd>wincmd l<CR>",           silent )
 
 --<< Move windows                           
-keymap( "n", "<A-h>",         "<CMD>wincmd H<CR>",           silent )
-keymap( "n", "<A-j>",         "<CMD>wincmd J<CR>",           silent )
-keymap( "n", "<A-k>",         "<CMD>wincmd K<CR>",           silent )
-keymap( "n", "<A-l>",         "<CMD>wincmd L<CR>",           silent )
+keymap( "n", "<A-h>",         "<Cmd>wincmd H<CR>",           silent )
+keymap( "n", "<A-j>",         "<Cmd>wincmd J<CR>",           silent )
+keymap( "n", "<A-k>",         "<Cmd>wincmd K<CR>",           silent )
+keymap( "n", "<A-l>",         "<Cmd>wincmd L<CR>",           silent )
 
 --<< Resize windows
-keymap( "n", "<C-Left>",      "<CMD>vertical resize -1<CR>", silent )
-keymap( "n", "<C-Right>",     "<CMD>vertical resize +1<CR>", silent )
-keymap( "n", "<C-Up>",        "<CMD>resize +1<CR>",          silent )
-keymap( "n", "<C-Down>",      "<CMD>resize -1<CR>",          silent )
+keymap( "n", "<C-Left>",      "<Cmd>vertical resize -1<CR>", silent )
+keymap( "n", "<C-Right>",     "<Cmd>vertical resize +1<CR>", silent )
+keymap( "n", "<C-Up>",        "<Cmd>resize +1<CR>",          silent )
+keymap( "n", "<C-Down>",      "<Cmd>resize -1<CR>",          silent )
 
 --<< Tabs
--- keymap( "n", "<Leader>t",     "<CMD>tabnew ",                silent )
-keymap( "n", "<Leader>,",     "<CMD>tabclose<CR>",           silent )
-keymap( "n", "<C-.>",         "<CMD>tabnext<CR>",            silent )
-keymap( "n", "<C-,>",         "<CMD>tabprevious<CR>",        silent )
-keymap( "n", "<A-.>",         "<CMD>+tabmove<CR>",           silent )
-keymap( "n", "<A-,>",         "<CMD>-tabmove<CR>",           silent )
+keymap( "n", "<Leader>.",     "<Cmd>tabnew<CR>",             silent )
+keymap( "n", "<Leader>,",     "<Cmd>tabclose<CR>",           silent )
+keymap( "n", "<C-.>",         "<Cmd>tabnext<CR>",            silent )
+keymap( "n", "<C-,>",         "<Cmd>tabprevious<CR>",        silent )
+keymap( "n", "<A-.>",         "<Cmd>+tabmove<CR>",           silent )
+keymap( "n", "<A-,>",         "<Cmd>-tabmove<CR>",           silent )
 
 --<< Plugin
-keymap( "n", "<C-p>c",        "<CMD>PackerCompile<CR>\
-                               <CMD>PackerInstall<CR>",      nosilent )
-keymap( "n", "<C-p>u",        "<CMD>PackerSync<CR>\
-                               <CMD>TSUpdate<CR>",           nosilent )
-keymap( "n", "<Leader>lm",    "<CMD>Mason<CR>",              nosilent )
+keymap( "n", "<C-p>c",        "<Cmd>PackerCompile<CR>\
+                               <Cmd>PackerInstall<CR>",      nosilent )
+keymap( "n", "<C-p>u",        "<Cmd>PackerSync<CR>\
+                               <Cmd>TSUpdate<CR>",           nosilent )
+keymap( "n", "<Leader>lm",    "<Cmd>Mason<CR>",              nosilent )
 
 --<< VISUAL MODE >>--
+-- TODO: adapt the move commands to <Cmd> instead of :
 
 --<< Normal
 keymap( "v", ">",             ">gv",                         silent )
 keymap( "v", "<",             "<gv",                         silent )
 keymap( "v", "p",             '"_dP',                        silent )
-keymap( "v", "<A-j>",         "<CMD>move .+1<CR>==",         silent )
-keymap( "v", "<A-k>",         "<CMD>move .-1<CR>==",         silent )
+keymap( "v", "<A-j>",         ":move .+1<CR>==",             silent )
+keymap( "v", "<A-k>",         ":move .-1<CR>==",             silent )
 
 --<< Block
-keymap( "x", "<A-j>",         "<CMD>move '>+1<CR>gv-gv",     silent )
-keymap( "x", "<A-k>",         "<CMD>move '<-2<CR>gv-gv",     silent )
+keymap( "x", "<A-j>",         ":move '>+1<CR>gv-gv",         silent )
+keymap( "x", "<A-k>",         ":move '<-2<CR>gv-gv",         silent )
 
 --<< ALTERNATIVES >>--
 
--- keymap( "n", "<Leader>v",     "<C-W>v<C-W>l<CMD>e ", options )
--- keymap( "n", "<Leader>s",     "<C-W>s<C-W>j<CMD>e ", options )
--- keymap( "x", "<A-h>", "<CMD>move '>+1<CR>gv-gv", options )       -- Need to move text laterally
--- keymap( "x", "<A-l>", "<CMD>move '<-2<CR>gv-gv", options )       -- Need to move text laterally
+-- keymap( "n", "<Leader>v",     "<C-W>v<C-W>l<Cmd>e ", options )
+-- keymap( "n", "<Leader>s",     "<C-W>s<C-W>j<Cmd>e ", options )
+-- keymap( "x", "<A-h>", "<Cmd>move '>+1<CR>gv-gv", options )       -- Need to move text laterally
+-- keymap( "x", "<A-l>", "<Cmd>move '<-2<CR>gv-gv", options )       -- Need to move text laterally
