246c359e7282cee4b27acb37706119837b3e15b9
Lasagnanator <d96a07@gmail.com>
2023-03-19 11:32:28 +0100

    Added autostart applications, removed bar code from rc.lua

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -22,7 +22,6 @@ local ruled = require("ruled")
                       require("awful.hotkeys_popup.keys")
 -- Error handling
                       require("modules.error")
-local gears = require("gears")
 
 --<< Modules
 -- local widgets = require("modules.widgets")
@@ -37,19 +36,19 @@ local theme_path = global.theme_path .. "theme.lua"
 beautiful.init(theme_path)
 
 --<< Autostart
--- local autostart = {
---     'udiskie',
---     'easyeffects --gapplication-service',
---     'picom --experimental-backend',
--- }
---
--- for _, cmd in pairs(autostart) do
---     awful.spawn.once(cmd)
--- end
+-- local autostart_script = gears.filesystem.get_configuration_dir() .. 'autostart.sh'
+-- awful.spawn.once(autostart_script)
 
-local autostart_script = gears.filesystem.get_configuration_dir() .. 'autostart.sh'
-awful.spawn.once(autostart_script)
+local autostart = {
+    "udiskie",
+    "easyeffects --gapplication-service",
+    "picom --experimental-backends",
+    "xss-lock " .. os.getenv("HOME") .. "/.scripts/lockscreen",
+}
 
+for _, cmd in pairs(autostart) do
+    awful.spawn.once(cmd)
+end
 
 --<< Bling
 -- local bling = require("bling")
@@ -79,124 +78,6 @@ end)
 require("modules.keybindings")
 require("modules.buttons")
 
--- --<< Wibar
--- -- Keyboard map indicator and switcher
--- ---@diagnostic disable-next-line: unused-local
--- local mykeyboardlayout = awful.widget.keyboardlayout()
---
--- -- Create a textclock widget
--- local mytextclock = wibox.widget.textclock()
---
--- screen.connect_signal("request::desktop_decoration", function(s)
---     -- Each screen has its own tag table.
---     awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
---
---     -- Create a promptbox for each screen
---     s.mypromptbox = awful.widget.prompt()
---
---     -- Create an imagebox widget which will contain an icon indicating which layout we're using.
---     -- We need one layoutbox per screen.
---     s.mylayoutbox = awful.widget.layoutbox {
---         screen  = s,
---         buttons = {
---             awful.button({ }, 1, function () awful.layout.inc( 1) end),
---             awful.button({ }, 3, function () awful.layout.inc(-1) end),
---             awful.button({ }, 4, function () awful.layout.inc(-1) end),
---             awful.button({ }, 5, function () awful.layout.inc( 1) end),
---         }
---     }
---
---     -- Create a taglist widget
---     s.mytaglist = awful.widget.taglist {
---         screen  = s,
---         filter  = awful.widget.taglist.filter.all,
---         buttons = {
---             awful.button({ }, 1, function(t) t:view_only() end),
---             awful.button({ modkey }, 1, function(t)
---                 if client.focus then
---                     client.focus:move_to_tag(t)
---                 end
---             end),
---             awful.button({ }, 3, awful.tag.viewtoggle),
---             awful.button({ modkey }, 3, function(t)
---                 if client.focus then
---                     client.focus:toggle_tag(t)
---                 end
---             end),
---             -- awful.button({ }, 4, function(t) awful.tag.viewprev(t.screen) end),
---             -- awful.button({ }, 5, function(t) awful.tag.viewnext(t.screen) end),
---         }
---     }
---
---     -- Custom taglist
---     s.mytaglist = awful.widget.taglist {
---         screen  = s,
---         filter  = awful.widget.taglist.filter.all,
---         buttons = {
---             awful.button({ }, 1, function(t) t:view_only() end),
---             awful.button({ modkey }, 1, function(t)
---                                             if client.focus then
---                                                 client.focus:move_to_tag(t)
---                                             end
---                                         end),
---             awful.button({ }, 3, awful.tag.viewtoggle),
---             awful.button({ modkey }, 3, function(t)
---                                             if client.focus then
---                                                 client.focus:toggle_tag(t)
---                                             end
---                                         end),
---             -- awful.button({ }, 4, function(t) awful.tag.viewprev(t.screen) end),
---             -- awful.button({ }, 5, function(t) awful.tag.viewnext(t.screen) end),
---         }
---     }
---
---     -- Create a tasklist widget
---     s.mytasklist = awful.widget.tasklist {
---         screen  = s,
---         filter  = awful.widget.tasklist.filter.currenttags,
---         buttons = {
---             awful.button({ }, 1, function (c)
---                 c:activate { context = "tasklist", action = "toggle_minimization" }
---             end),
---             awful.button({ }, 3, function() awful.menu.client_list { theme = { width = 250 } } end),
---             awful.button({ }, 4, function() awful.client.focus.byidx(-1) end),
---             awful.button({ }, 5, function() awful.client.focus.byidx( 1) end),
---         }
---     }
---
---     -- Create the wibox
---     s.mywibox = awful.wibar {
---         position = "top",
---         screen   = s,
---         widget   = {
---             layout = wibox.layout.align.horizontal,
---             expand = "none",
---             { -- Left widgets
---                 layout = wibox.layout.fixed.horizontal,
---                 s.mylayoutbox,
---                 mylauncher,
---                 s.mypromptbox,
---             },
---             { -- Middle widget
---                 layout = wibox.layout.align.horizontal,
---                 -- expand = "none",
---                 s.mytaglist,
---                 -- s.mytasklist,
---             },
---             { -- Right widgets
---                 layout = wibox.layout.fixed.horizontal,
---                 -- mykeyboardlayout,
---                 wibox.widget.systray(),
---                 widgets.mem,
---                 widgets.cpu,
---                 widgets.temp,
---                 widgets.bat,
---                 mytextclock,
---             },
---         }
---     }
--- end)
---
 --<< Client management
 client.connect_signal("manage", function(c)
      if not awesome.startup then awful.client.setslave(c) end
@@ -261,3 +142,4 @@ end)
 --client.connect_signal("mouse::enter", function(c)
 --    c:activate { context = "mouse_enter", raise = false }
 --end)
+
